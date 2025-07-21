c90d0f54d0555e4bfb6f31ac1807f04213f80a17
Mark1-togo <macoduor1@gmail.com>
2025-07-21 15:19:09 +0300

    strings and concatenation

---
+++ TriviaBot/trivia.js
@@ -0,0 +1,26 @@
+console.log("Hello! I'm your coding fun fact guide!");
+let botName = "John";
+let botLocation = "Oyugis";
+let favoriteLanguage = "JavaScript";
+console.log(
+  "my name is " +
+    botName +
+    ", I am located in " +
+    botLocation +
+    ", and my favorite programming language is " +
+    favoriteLanguage +
+    "."
+);
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+let codingFact =
+  favoriteLanguage + " is a versatile language used for web development, ";
+console.log(codingFact);
+codingFact = favoriteLanguage + " is also used for server-side development.";
+console.log(codingFact);
+codingFact =
+  "It was fun sharing these facts with you. Goodbye! - " +
+  botName +
+  " from " +
+  botLocation +
+  ".";
+console.log(codingFact);
---
+++ strings-concatenation/strings.js
@@ -0,0 +1,48 @@
+let adjective;
+adjective = "Good";
+let noun = " boy";
+let verb = "jumped";
+let place = "house";
+let adjective2 = "aged";
+let noun2 = "beef";
+
+let firstStory =
+  "Once upon a time, there was a " +
+  adjective +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  "." +
+  "The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("First story: " + firstStory);
+adjective = "bad";
+noun = " girl";
+verb = "cry";
+place = "city";
+adjective2 = "tall";
+noun2 = "chicken";
+let secondStory =
+  "Once upon a time, there was a " +
+  adjective +
+  noun +
+  " who loved to eat " +
+  noun2 +
+  "." +
+  "The " +
+  noun +
+  " lived in a " +
+  place +
+  " and had " +
+  adjective2 +
+  " nostrils that blew fire when it was " +
+  verb +
+  ".";
+console.log("Second story: " + secondStory);
