2d888727beb09f9b412ecce4a9c190c5a14c0307
Mark1-togo <macoduor1@gmail.com>
2025-07-24 15:52:27 +0300

    NaN parseFloat() parseInt()

--- math object/script.js
+++ math object/script.js
@@ -51,3 +51,48 @@ if (selectedFortune === 1) {
 } else if (selectedFortune === 5) {
   console.log(fortune5);
 }
+/*
+NaN represents undefined numerical result. also considered a type of number
+isNaN() a function used to determine whether a value is NaN or not.
+Number.isNaN() provides a more reliable way to check for NaN values, especially in cases where type coercion might lead to unexpected results with the global isNaN() function. In practice, when dealing with numerical operations or user inputs that should be numbers, it's often necessary to check for NaN to handle errors or unexpected inputs gracefully
+Number.isNaN(value) checks if a value is exactly NaN
+*/
+
+console.log(isNaN("37")); // false
+console.log(isNaN(true)); //false
+console.log(isNaN("")); // false empty string converted to 0
+console.log(isNaN(NaN)); // true
+console.log(isNaN("45.3")); // false "45.3" is converted to a number
+function division(a, b) {
+  let result = a / b;
+  if (Number.isNaN(result)) {
+    return `Error: Addison resulted in NaN`;
+  }
+  return result;
+}
+console.log(division(2, 2)); // 1
+console.log(division(10, 0)); // infinity
+console.log(division(0, 0)); // Error: Addison resulted in NaN
+
+//In JavaScript, NaN === NaN is false because NaN (Not-a-Number) is defined as a value that is not equal to anything, even itself.
+//parseFloat() returns floating point number from a string  If it can't find a valid number at the start of the string, it returns NaN (Not a Number).
+console.log(parseInt("42")); // 42
+console.log(parseFloat("3.14.5")); // Output: 3.14
+console.log(parseFloat("3.14 abc")); // Output: 3.14
+//parseInt() returns a string to an integer
+
+console.log(parseInt("42px")); // Output: 42
+console.log(parseInt("3.14")); // Output: 3
+console.log(parseInt("-42")); // Output: -42
+console.log(parseInt("abc123")); // Output: NaN
+console.log(parseInt("10.99")); // 10
+
+/* 
+.toFixed()
+method is a built-in JavaScript function that formats a number using fixed-point notation. 
+It's particularly useful when you need to control the number of decimal places in a number, especially for displaying currency values or when working with precise measurements.
+*/
+console.log((3.142).toFixed(2)); // 3.14
+let num = 5.678;
+console.log(num.toFixed(1));
+console.log(1 / 0);
