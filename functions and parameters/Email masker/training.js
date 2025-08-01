f5ff86c3a701508e0270f1f65f9ed50130e8a332
Mark1-togo <macoduor1@gmail.com>
2025-08-01 23:52:18 +0300

    functions training

--- functions and parameters/Email masker/training.js
+++ functions and parameters/Email masker/training.js
@@ -3,7 +3,7 @@ javaScript functions
 Using asterisk to mask the middle part of an email address 
 johndoe1@example.com 
 j******1@example.com
-*/
+
 function maskedEmail(mail) {
   let atIndex = mail.indexOf("@");
 
@@ -25,3 +25,27 @@ function maskedEmail(mail) {
 let mail = "johndoe1@example.com";
 console.log(maskedEmail(mail));
 console.log(maskedEmail("steve2@example.com"));
+*/
+
+function emailMasker(mail) {
+  let indexAt = mail.indexOf("@");
+
+  const localPart = mail.slice(0, indexAt);
+  const domainPart = mail.slice(indexAt);
+
+  const maskedPart =
+    localPart[0] +
+    "*".repeat(localPart.length - 2) +
+    localPart[localPart.length - 1];
+
+  return maskedPart + domainPart;
+}
+console.log(emailMasker("macoduor1@gmail.com"));
+
+let stringExample = "Trying the slice method";
+console.log(stringExample.slice(0, 10));
+function passwordHider(password) {
+  return "*".repeat(password.length);
+}
+
+console.log(passwordHider("pass"));
