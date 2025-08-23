ad88c27a504450e2d6b81f31704a29db8611ce62
Mark1-togo <macoduor1@gmail.com>
2025-08-23 13:01:44 +0300

    Build a Confirm the Ending Tool

--- functions and parameters/string checker/script.js
+++ functions and parameters/string checker/script.js
@@ -1,8 +1,19 @@
 function confirmEnding(str1, str2) {
-  if (str1.length - 1 === str1.indexOf(str2)) {
-    return true;
-  } else {
-    return false;
-  }
+  return str1.slice(-str2.length) === str2;
 }
-console.log(confirmEnding("Tony", "n"));
+confirmEnding("Bastian", "n");
+confirmEnding("Congratulation", "on");
+confirmEnding("Connor", "n");
+confirmEnding(
+  "Walking on water and developing software from a specification are easy if both are frozen",
+  "specification"
+);
+confirmEnding("He has to give me a new name", "name");
+confirmEnding("Open sesame", "same");
+confirmEnding("Open sesame", "sage");
+confirmEnding("Open sesame", "game");
+confirmEnding(
+  "If you want to save our world, you must hurry. We don't know how much longer we can withstand the nothing",
+  "mountain"
+);
+confirmEnding("Abstraction", "action");
